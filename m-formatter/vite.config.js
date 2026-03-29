befc78c51a61e9dff263e131845e51eb51af062f
Raúl <raulr8128@gmail.com>
2026-03-29 17:07:08 -0300

    chore: configuración final de base path para GitHub Pages

--- m-formatter/vite.config.js
+++ m-formatter/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Esto arregla la pantalla en blanco
+  base: '/m-formatter/', // <-- ESTO ES LO MÁS IMPORTANTE
 })
