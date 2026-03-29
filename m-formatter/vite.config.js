5285d938fe77d1674c04187c64c6bb6e3622345c
Raúl <raulr8128@gmail.com>
2026-03-29 17:43:13 -0300

    fix: ajustar base path a mayúsculas para GitHub

--- m-formatter/vite.config.js
+++ m-formatter/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/m-formatter/', // <-- ESTO ES LO MÁS IMPORTANTE
+  base: '/M-formatter/', // <-- ESTO ES LO MÁS IMPORTANTE
 })
